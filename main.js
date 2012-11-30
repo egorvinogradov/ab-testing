b4d4b2ab22bf2f7d095056fdc8b7c70166abc8b8
Egor Vinogradov <egor@ostrovok.ru>
2012-11-30 16:53:49 +0400

    debug

--- main.js
+++ main.js
@@ -4,10 +4,19 @@ var url = require('url');
 
 console._log = console.log;
 console.log = function(){
-    Array.prototype.unshift.call(arguments, '[' + new Date() + ']:\n');
+    Array.prototype.unshift.call(arguments, new Date() + ':\n');
     Array.prototype.push.call(arguments, '\n');
     console._log.apply({}, arguments);
 };
+
+$ = {
+    proxy: function(func, context){
+        return function(){
+            func.apply(context, arguments);
+        };
+    }
+};
+
 
 /* Router */
 
@@ -16,33 +25,37 @@ var Router = function(){
 };
 
 Router.prototype = {
-    experiments: {
-        pathnameRegExp: /\/experiments.*/,
-        callback: function(options){
-            console.log('Experiments:', options.request.url);
+    routes: {
+        experiments: {
+            pathnameRegExp: /\/experiments.*/,
+            callback: function(options){
+                console.log('Experiments:', options.request.url);
+            }
+        },
+        group: {
+            pathnameRegExp: /\/group.*/,
+            callback: function(options){
+                console.log('Group:', options.request.url);
+            }
+        },
+        error: {
+            pathnameRegExp: /\/error.*/,
+            callback: function(options){
+                console.log('Error:', options.request.url);
+            }
+        },
+        track: {
+            pathnameRegExp: /\/track.*/,
+            callback: function(options){
+                console.log('Track:', options.request.url);
+            }
         }
     },
-    group: {
-        pathnameRegExp: /\/group.*/,
-        callback: function(options){
-            console.log('Group:', options.request.url);
-        }
-    },
-    error: {
-        pathnameRegExp: /\/error.*/,
-        callback: function(options){
-            console.log('Error:', options.request.url);
-        }
-    },
-    track: {
-        pathnameRegExp: /\/track.*/,
-        callback: function(options){
-            console.log('Track:', options.request.url);
-        }
-    },
-    staticFiles: {
-        callback: function(options){
-            console.log('Static file:', options.request.url);
+    defaults: {
+        staticFiles: {
+            callback: function(options){
+                console.log('Static file:', options.request.url);
+            }
         }
     }
 };
@@ -58,7 +71,7 @@ Server.prototype = {
     initialize: function(router, port){
         this.router = router;
         this.port = port;
-        this.server = http.createServer(this.onRequest);
+        this.server = http.createServer($.proxy(this.onRequest, this));
         this.server.listen(port);
     },
     onRequest: function(request, response){
@@ -76,13 +89,13 @@ Server.prototype = {
     },
     selectRoute: function(options){
         var requestPathname = url.parse(options.request.url).pathname;
-        for ( var route in this.router ) {
-            if ( this.router[route].pathnameRegExp.test(requestPathname) ) {
-                this.router[route].callback(options);
+        for ( var routeName in this.router.routes ) {
+            if ( this.router.routes[routeName].pathnameRegExp.test(requestPathname) ) {
+                this.router.routes[routeName].callback(options);
                 return;
             }
         }
-        this.router.staticFiles.callback(options);
+        this.router.defaults.staticFiles.callback(options);
     },
     onSuccess: function(response, data){
         response.writeHead(200, {
